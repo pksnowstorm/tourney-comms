7215084d6c8a78a1a1653d38dbaef709b6daa0ca
pksnowstorm <enguyen21@gmail.com>
2023-04-10 15:19:41 -0500

    add in register and login and home page

--- server.js
+++ server.js
@@ -4,22 +4,38 @@ const methodOverride = require('method-override');
 const mongoose = require ('mongoose');
 const app = express();
 const db = mongoose.connection;
-const PORT = process.env.PORT || 3000;
+const session = require('express-session');
 const MONGODB_URL = process.env.MONGODB_URL;
 mongoose.connect(MONGODB_URL , {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
 });
-db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongod connected'));
-db.on('disconnected', () => console.log('mongod disconnected'));
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
 app.use(methodOverride('_method'));
-app.get('/' , (req, res) => {
-    res.send('Hello World!');
-  });
+const userController = require('./controllers/users');
+app.use('/users', userController);
+const sessionsController = require('./controllers/sessions');
+app.use('/sessions', sessionsController);
+app.get('/', (req, res) => {
+	if (req.session.currentUser) {
+		res.render('dashboard.ejs', {
+			currentUser: req.session.currentUser
+		});
+	} else {
+		res.render('index.ejs', {
+			currentUser: req.session.currentUser
+		});
+	}
+});
+const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log('express is listening on:', PORT));
