f7afdc32ce6790a5ca8b142dd3cffe541c4c7f13
pksnowstorm <enguyen21@gmail.com>
2023-04-11 16:07:52 -0500

    rearrange nav locations

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ app.get('/', (req, res) => {
 	}
 });
 const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log('express is listening on:', PORT));
+  app.listen(PORT, () => console.log(`express is listening on: ${PORT}`));
