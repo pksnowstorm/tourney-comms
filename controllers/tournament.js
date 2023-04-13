e0609e533470655cf340531c5d988d50213de908
pksnowstorm <enguyen21@gmail.com>
2023-04-13 13:31:44 -0500

    reorganize tournament controllers route

--- controllers/tournament.js
+++ controllers/tournament.js
@@ -3,6 +3,51 @@ const tournamentRouter = express.Router();
 const Tournament = require('../models/tournament.js');
 const User = require('../models/user.js');
 
+//Index
+tournamentRouter.get('/', async (req, res) => {
+    await Tournament.find({}, (error, allTournament) => {
+        res.render('tournament/index.ejs', {
+            tournament: allTournament,
+            currentUser: req.session.currentUser
+        })
+    })
+})
+
+//Edit
+tournamentRouter.get('/:id/edit', async (req, res) => {
+    await Tournament.findById(req.params.id, (err, foundTournament) => {
+		res.render('tournament/edit.ejs', {
+            tournament: foundTournament,
+            currentUser: req.session.currentUser,
+        })
+	})
+})
+
+
+//Delete
+tournamentRouter.delete('/:id', async (req, res) => {
+    await Tournament.findByIdAndRemove(req.params.id)
+    res.redirect('/')
+})
+
+//Update
+tournamentRouter.put('/:id', async (req, res) => {
+    if (req.body.openToPublic === 'on') {
+		req.body.openToPublic = true;
+	} else {
+		req.body.openToPublic = false;
+	}
+    await Tournament.findByIdAndUpdate(req.params.id, req.body,
+        {
+          new: true,
+        },
+        (error, updatedTournament) => {
+            res.redirect(`/tournament/${req.params.id}`)
+        }
+    )
+})
+
+//Create
 tournamentRouter.post('/', (req, res) => {
     if (req.body.openToPublic === 'on') {
 		req.body.openToPublic = true;
@@ -14,45 +59,6 @@ tournamentRouter.post('/', (req, res) => {
     });
 });
 
-tournamentRouter.get('/', (req, res) => {
-    Tournament.find({}, (error, allTournament) => {
-        res.render('tournament/index.ejs', {
-            tournament: allTournament,
-            currentUser: req.session.currentUser
-        })
-    })
-})
-
-tournamentRouter.get('/:id/edit', (req, res) => {
-    Tournament.findById(req.params.id, (err, foundTournament) => {
-		res.render('tournament/edit.ejs', {
-            tournament: foundTournament,
-            currentUser: req.session.currentUser,
-        })
-	})
-})
-
-tournamentRouter.delete('/:id', async (req, res) => {
-    await Tournament.findByIdAndRemove(req.params.id)
-    res.redirect('/')
-})
-
-tournamentRouter.put('/:id', (req, res) => {
-    if (req.body.openToPublic === 'on') {
-		req.body.openToPublic = true;
-	} else {
-		req.body.openToPublic = false;
-	}
-    Tournament.findByIdAndUpdate(req.params.id, req.body,
-        {
-          new: true,
-        },
-        (error, updatedTournament) => {
-            res.redirect(`/tournament/${req.params.id}`)
-        }
-    )
-})
-
 // New (Registration page)
 tournamentRouter.get('/new', (req, res) => {
 	res.render('tournament/new.ejs', {
@@ -61,6 +67,7 @@ tournamentRouter.get('/new', (req, res) => {
 	});
 });
 
+//Show
 tournamentRouter.get('/:id', (req, res) => {
     Tournament.findById(req.params.id, (err, foundTournament) => {
 		res.render('tournament/show.ejs', {
