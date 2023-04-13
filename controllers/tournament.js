db08b97ea05761667d89aeff020542ca7be6198a
pksnowstorm <enguyen21@gmail.com>
2023-04-13 06:20:39 -0500

    add user to tournament controller

--- controllers/tournament.js
+++ controllers/tournament.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const tournamentRouter = express.Router();
 const Tournament = require('../models/tournament.js');
+const User = require('../models/user.js');
 
 // New (Registration page)
 tournamentRouter.get('/new', (req, res) => {
 	res.render('tournament/new.ejs', {
-		currentTournament: req.session.currentTournament
+		currentTournament: req.session.currentTournament,
+        currentUser: req.session.currentUser
 	});
 });
 
@@ -19,6 +21,7 @@ tournamentRouter.get('/', (req, res) => {
     Tournament.find({}, (error, allTournament) => {
         res.render('tournament/index.ejs', {
             tournament: allTournament,
+            currentUser: req.session.currentUser
         })
     })
 })
@@ -27,6 +30,7 @@ tournamentRouter.get('/:id', (req, res) => {
     Tournament.findById(req.params.id, (err, foundTournament) => {
 		res.render('tournament/show.ejs', {
             tournament: foundTournament,
+            currentUser: req.session.currentUser
         })
 	})
 })
